e9f3002f0bf7e5f71d14f12f7ffa9a746a35430d
Rodrigo Henrique <rodrigohenrique987@gmail.com>
2025-10-25 21:29:10 -0300

    .

---
+++ Bullet-Heaven/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,31 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance { get; private set; }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
